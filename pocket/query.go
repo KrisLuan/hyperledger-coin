fd53ee5c32068575f33da9a7262832cf1ed1c943
hustfoam <1060762481@qq.com>
2018-01-30 16:43:57 +0800

    1. 修改queryAddrs方法，返回总资产和一个防重放nounce（用复合键方式处理高并发问题） 2. 增加方法queryBalance，只返回总积分

--- pocket/query.go
+++ pocket/query.go
@@ -14,13 +14,22 @@ func (t *PocketChaincode)queryAddrs(store Store, args []string) pb.Response {
 	}
 
 	addr := args[1]
-	pocket, err := store.GetPocket(addr)
+	assets, nounce, err := store.GetAllAssets(addr)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	s := strconv.FormatInt(pocket.Balance, 10)
-	protobytebase64 := make([]byte, base64.StdEncoding.EncodedLen(len(s)))
-	base64.StdEncoding.Encode(protobytebase64, []byte(s))
+
+	queryResult := new(QueryResult)
+	queryResult.Balance = assets
+	//balance 作为nounce，保证不能重放
+	queryResult.Nounce = nounce
+
+	data, err := proto.Marshal(queryResult)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	protobytebase64 := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	base64.StdEncoding.Encode(protobytebase64, []byte(data))
 	return shim.Success(protobytebase64)
 }
 
@@ -34,6 +43,9 @@ func (t *PocketChaincode)queryStatistics(store Store, args []string) pb.Response
 		return shim.Error(err.Error())
 	}
 	data, err := proto.Marshal(pointInfo)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 	protobytebase64 := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
 	base64.StdEncoding.Encode(protobytebase64, []byte(data))
 	return shim.Success(protobytebase64)
@@ -50,7 +62,27 @@ func (t *PocketChaincode)queryPointkind(store Store, args []string) pb.Response 
 	}
 
 	data, err := proto.Marshal(pointKind)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 	protobytebase64 := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
 	base64.StdEncoding.Encode(protobytebase64, []byte(data))
+	return shim.Success(protobytebase64)
+}
+
+func (t *PocketChaincode)queryBalance(store Store, args []string) pb.Response {
+	if len(args) != 2 {
+		return shim.Error(ErrInvalidArgs.Error())
+	}
+
+	addr := args[1]
+	assets, _, err := store.GetAllAssets(addr)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	s := strconv.FormatInt(assets, 10)
+	protobytebase64 := make([]byte, base64.StdEncoding.EncodedLen(len(s)))
+	base64.StdEncoding.Encode(protobytebase64, []byte(s))
 	return shim.Success(protobytebase64)
 }
