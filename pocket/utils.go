4b50b4f0a79f3b4deb9fa16a8a2ae083878367b1
hustfoam <1060762481@qq.com>
2018-01-30 16:48:46 +0800

    增加工具，解析txMaps

--- pocket/utils.go
+++ pocket/utils.go
@@ -52,6 +52,18 @@ func ParsePointKind(data []byte) (*PointKind, error) {
 	return pointKind, nil
 }
 
+func ParseTxMaps(data []byte) (*TXMap, error) {
+	if data == nil || len(data) == 0 {
+		return nil, ErrKeyNoData
+	}
+
+	txMaps := new(TXMap)
+	if err := proto.Unmarshal(data, txMaps); err != nil {
+		return nil, err
+	}
+	return txMaps, nil
+}
+
 func IsValidAddr(addr string, pubkey string) bool {
 	return !strings.Contains(addr, "_")
 }
