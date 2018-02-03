3de123028980bf072d975cef552266586d71e81e
hustfoam <1060762481@qq.com>
2018-02-03 15:37:14 +0800

    修改真实可用的常量

--- pocket/constant.go
+++ pocket/constant.go
@@ -8,9 +8,10 @@ var (
 
 const (
 	DefaultPocketKind string	= "default"
-	InitAddr string				= "foam"
+	InitAddr string				= "caRNGB62WSf3AiCToXn2iCGU1RJ9tr4Vu"
 	InitTotalPoint int64		= 20000000*100000000
-	InitPubkey string			= "fafasdfasdfas"
+	InitPubkey string			= "BNFHetucyZHP1FtrtRz0UhWwSfIfsHY0NA+2ZJuRS+KOIClJEwjxnzSUKhRqyz2tt/XfrCykxkb7Qrm2gjeMJRU="
+	InitPrikey string			= "53DAFF852B6207DD92936541115BCC703C2A6062F7B7C0B497D30475156D9140"
 	//联合键注意，addr是不能包含‘_’的
 	CompositeIndexName string	= "foam_point"
 )
